b240585af031a67432dea3f2c1be3b27f9346e5d
Jinkun Chen <i@jinkunchen.com>
2026-02-16 15:09:35 -0400

    feat(ui): update seasonal ornament layout with fixed desktop sizes

--- components/festival-overlay.tsx
+++ components/festival-overlay.tsx
@@ -2,26 +2,43 @@ const FESTIVAL_LANTERN_SRC =
   "https://cdn.jinkunchen.com/web_image/year_of_horse/lantern.png";
 const FESTIVAL_POETRY_SRC =
   "https://cdn.jinkunchen.com/web_image/year_of_horse/poetry.png";
+const FESTIVAL_KOI_SRC =
+  "https://cdn.jinkunchen.com/web_image/year_of_horse/koi.png";
 
 export default function FestivalOverlay() {
   return (
     <aside className="festival-overlay" aria-hidden="true">
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img
-        className="festival-overlay__lantern"
-        src={FESTIVAL_LANTERN_SRC}
-        alt=""
-        loading="eager"
-        decoding="async"
-      />
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img
-        className="festival-overlay__poetry"
-        src={FESTIVAL_POETRY_SRC}
-        alt=""
-        loading="eager"
-        decoding="async"
-      />
+      <section className="festival-overlay__left">
+        <div className="festival-overlay__lantern-block" />
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          className="festival-overlay__lantern"
+          src={FESTIVAL_LANTERN_SRC}
+          alt=""
+          loading="eager"
+          decoding="async"
+        />
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          className="festival-overlay__poetry"
+          src={FESTIVAL_POETRY_SRC}
+          alt=""
+          loading="eager"
+          decoding="async"
+        />
+      </section>
+
+      <section className="festival-overlay__right">
+        <div className="festival-overlay__koi-block" />
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          className="festival-overlay__koi"
+          src={FESTIVAL_KOI_SRC}
+          alt=""
+          loading="lazy"
+          decoding="async"
+        />
+      </section>
     </aside>
   );
 }
